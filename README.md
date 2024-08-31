4ae01c508559f4f8b58461b5d7c14b337c59325e
reemgaber213@gmail.com <reemgaber213@gmail.com>
2024-08-31 20:33:38 +0300

    gitecomm

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Assignment4
+# EcommAssignment
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Assignment4' title`, () => {
+  it(`should have the 'EcommAssignment' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Assignment4');
+    expect(app.title).toEqual('EcommAssignment');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Assignment4');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, EcommAssignment');
   });
 });
